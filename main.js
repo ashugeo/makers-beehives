08d71231b6753256178e5ae725e6a5b9c9afbeef
ashugeo <hugo.saintemarie@gmail.com>
2019-10-26 15:57:23 +0200

    Hotfix

--- main.js
+++ main.js
@@ -95,6 +95,7 @@ function createChart($el, json) {
     }
 
     for (let i = 10; i >= 0; i -= 1) {
+        if (!json[i] || !json[i].values[0].formattedValue) continue;
         let date = json[i].values[0].formattedValue;
         date = `${date.split('/')[0]}/${date.split('/')[1]}`
         data.labels.push(date);
